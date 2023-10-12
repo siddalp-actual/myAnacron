9d1a4ac4cf28d821f383bad3c014a71e5fcec04c
siddalp-actual <76445019+siddalp-actual@users.noreply.github.com>
2023-10-12 16:38:08 +0100

    had forgotten that the scripts were links, dereference them

--- daily/createCsvFiles.sh
+++ daily/createCsvFiles.sh
@@ -1,1 +1,8 @@
-/home/siddalp/Dropbox/pgm/gnucash/createCsvFiles.sh
+#! /bin/bash
+cd ~/Dropbox/pgm/gnucash
+#export PYENV_VERSION="gnrlPy"
+#echo $PYENV_VERSION
+#/home/siddalp/.pyenv/bin/pyenv init -
+#/home/siddalp/.pyenv/bin/pyenv virtualenv init -
+#./extractGnucashDataV3.py
+/home/siddalp/.pyenv/versions/gnrlPy/bin/python3 ./extractGnucashDataV3.py $*
--- daily/dailySave.sh
+++ daily/dailySave.sh
@@ -1,1 +1,4 @@
-/home/siddalp/pgm/dailySave.sh
+#! /bin/bash
+cd ~/Dropbox/music
+find ~/audio -name *.ly | tar -cvzf lilypond\ backup.tar.gz -T -
+gpg --batch --yes --default-recipient-self --output ~/Dropbox/pgm/gnucash/PetesSQL.gnucash.gpg --encrypt ~/gnucash/PetesSQL.gnucash
