791dc77660b73db22488bab04d02774754d46001
siddalp-actual <76445019+siddalp-actual@users.noreply.github.com>
2023-11-07 12:36:22 +0000

    add additional files to capture gregorio transcriptions

--- daily/dailySave.sh
+++ daily/dailySave.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 cd ~/Dropbox/music
-find ~/audio -name *.ly | tar -cvzf lilypond\ backup.tar.gz -T -
+find ~/audio -name *.ly -o -name *.tex -o -name *.gabc | tar -cvzf lilypond\ backup.tar.gz -T -
 gpg --batch --yes --default-recipient-self --output ~/Dropbox/pgm/gnucash/PetesSQL.gnucash.gpg --encrypt ~/gnucash/PetesSQL.gnucash
